a138572a5ae687902be65575439b7b3ded707063
ArduDarugutni <theviel77@gmail.com>
2023-03-21 13:58:31 +0700

    Upload Tugas PHP 1

--- tugasphp1.php
+++ tugasphp1.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Jajar Genjang</title>
+    <title>Form Jajar Genjang</title>
 </head>
 
 <body style="background-color: lightblue; text-align:left;">
