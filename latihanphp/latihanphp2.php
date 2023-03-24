926d9220e676f1b7441158e14712bc70666d41da
ArduDarugutni <theviel77@gmail.com>
2023-03-24 17:19:19 +0700

    Upload latihan php

---
+++ latihanphp/latihanphp2.php
@@ -0,0 +1,10 @@
+<?php
+$nama = "ardu";
+$totalBelanja = 300000;
+$keterangan = "";
+
+if($totalBelanja > 100000){
+    $keterangan = "Selamat $nama Anda dapat hadiah";
+}else {
+    $keterangan = "";
+}
